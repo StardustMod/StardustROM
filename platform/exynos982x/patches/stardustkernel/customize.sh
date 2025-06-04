9409d1c98e4882decbfd1f52294e5cca054c1d0f
oItsMineZ <135024283+oItsMineZ@users.noreply.github.com>
2025-06-04 01:31:17 -0700

    platform: exynos982x: patches: add StardustKernel

---
+++ platform/exynos982x/patches/stardustkernel/customize.sh
@@ -0,0 +1,52 @@
+# [
+REPO="https://raw.githubusercontent.com/StardustMod/build/refs/heads/exynos9820/Preload"
+KERNEL="$REPO/Kernel"
+MANAGER="${KERNEL}SU-Next.apk"
+
+REPLACE_KERNEL_BINARIES()
+{
+    [ -d "$TMP_DIR" ] && rm -rf "$TMP_DIR"
+    mkdir -p "$TMP_DIR"
+
+    ZIP_LINK="$KERNEL/${DEVICE_CODENAME}.zip"
+    echo "Downloading $(basename "$ZIP_LINK")"
+    curl -Ls -o "$TMP_DIR/Kernel.zip" "$ZIP_LINK"
+
+    echo "Extracting kernel binaries"
+    rm -f "$WORK_DIR/kernel/"*.img
+    unzip -q -j "$TMP_DIR/Kernel.zip" \
+        "files/boot.img" "files/dtbo.img" "files/dtb.img" \
+        -d "$WORK_DIR/kernel"
+
+    rm -rf "$TMP_DIR"
+}
+
+ADD_MANAGER_APK_TO_PRELOAD()
+{
+    # https://github.com/tiann/KernelSU/issues/886
+    local APK_PATH="system/preload/KernelSU-Next/com.rifsxd.ksunext-star==/base.apk"
+
+    echo "Adding KernelSU-Next.apk to preload apps"
+    mkdir -p "$WORK_DIR/system/$(dirname "$APK_PATH")"
+    curl -L -s -o "$WORK_DIR/system/$APK_PATH" -z "$WORK_DIR/system/$APK_PATH" "$MANAGER"
+
+    sed -i "/system\/preload/d" "$WORK_DIR/configs/fs_config-system" \
+        && sed -i "/system\/preload/d" "$WORK_DIR/configs/file_context-system"
+    while read -r i; do
+        FILE="$(echo -n "$i"| sed "s.$WORK_DIR/system/..")"
+        [ -d "$i" ] && echo "$FILE 0 0 755 capabilities=0x0" >> "$WORK_DIR/configs/fs_config-system"
+        [ -f "$i" ] && echo "$FILE 0 0 644 capabilities=0x0" >> "$WORK_DIR/configs/fs_config-system"
+        FILE="$(echo -n "$FILE" | sed 's/\./\\./g')"
+        echo "/$FILE u:object_r:system_file:s0" >> "$WORK_DIR/configs/file_context-system"
+    done <<< "$(find "$WORK_DIR/system/system/preload")"
+
+    rm -f "$WORK_DIR/system/system/etc/vpl_apks_count_list.txt"
+    while read -r i; do
+        FILE="$(echo "$i" | sed "s.$WORK_DIR/system..")"
+        echo "$FILE" >> "$WORK_DIR/system/system/etc/vpl_apks_count_list.txt"
+    done <<< "$(find "$WORK_DIR/system/system/preload" -name "*.apk" | sort)"
+}
+# ]
+
+REPLACE_KERNEL_BINARIES
+ADD_MANAGER_APK_TO_PRELOAD
