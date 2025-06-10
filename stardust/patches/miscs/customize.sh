d302d4d57ac7867d2c27a2304a03ed911837114e
oItsMineZ <135024283+oItsMineZ@users.noreply.github.com>
2025-06-09 18:16:34 -0700

    stardust: patches: miscs: disable FRP

--- stardust/patches/miscs/customize.sh
+++ stardust/patches/miscs/customize.sh
@@ -1,2 +1,5 @@
 # Set Build Display ID
 SET_PROP "system" "ro.build.display.id" "StardustROM-$ROM_VERSION-$DEVICE_CODENAME-`date +"%Y%m%d"` ($(GET_PROP "system" "ro.build.display.id"))"
+
+# Disable FRP
+SET_PROP "vendor" "ro.frp.pst" ""
