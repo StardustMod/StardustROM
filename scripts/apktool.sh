f1122260421e4682eac0b3f63614e94bfda7c72e
oItsMineZ <135024283+oItsMineZ@users.noreply.github.com>
2025-06-13 17:08:21 -0700

    scripts: sign apk on SecSettings

--- scripts/apktool.sh
+++ scripts/apktool.sh
@@ -79,9 +79,8 @@ BUILD()
     local FILE_NAME
     FILE_NAME="$(basename "$INPUT_FILE")"
 
-    if [[ "$INPUT_FILE" == *".apk" ]]; then
+    if [[ "$INPUT_FILE" == *SecSettings.apk ]]; then
         local CERT_PREFIX="aosp"
-        $ROM_IS_OFFICIAL && CERT_PREFIX="unica"
 
         LOG "- Signing ${INPUT_FILE//$WORK_DIR/}"
         EVAL "signapk \"$SRC_DIR/security/${CERT_PREFIX}_platform.x509.pem\" \"$SRC_DIR/security/${CERT_PREFIX}_platform.pk8\" \"$OUTPUT_PATH/dist/$FILE_NAME\" \"$OUTPUT_PATH/dist/temp.apk\"" || exit 1
